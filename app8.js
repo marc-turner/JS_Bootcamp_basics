4b8524573723603f6c170095f4ccf214c82f724a
Marc Turner <marc.t1967@gmail.com>
2020-01-03 22:37:44 -0500

    add map() examples

--- app8.js
+++ app8.js
@@ -1,6 +1,6 @@
 // forEach();
 
-const numbers = [20, 21, 22, 23, 24, 25, 26, 27];
+// const numbers = [20, 21, 22, 23, 24, 25, 26, 27];
 
 // numbers.forEach(function (num) {
 //     console.log(num * 2);
@@ -12,7 +12,55 @@ const numbers = [20, 21, 22, 23, 24, 25, 26, 27];
 
 // numbers.forEach(printTriple);
 
-numbers.forEach(function (idx, num) {
-    console.log(num, idx);
+// numbers.forEach(function (idx, num) {
+//     console.log(num, idx);
+// });
+
+//////////////////////////////////////
+// map()
+
+
+const numbers = [20, 21, 22, 23, 24, 25, 26, 27];
+const words = ['asap', 'byob', 'rsvp', 'diy'];
+
+const doubles = numbers.map(function (num) {
+    return num * 2;
 });
 
+const numDetail = numbers.map(function (n) {
+    return {
+        value: n,
+        isEven: n % 2 === 0
+    }
+});
+
+const newWord = words.map(function (word) {
+    return word.toUpperCase().split("").join(".");
+});
+
+const records = [
+    {
+        title: "Revolver",
+        artist: "The Beatles",
+        year: 1966
+    },
+    {
+        title: "The Police",
+        artist: "The Police",
+        year: 1980
+    },
+    {
+        title: "OK Computer",
+        artist: "Radiohead",
+        year: 1999
+    },
+    {
+        title: "Hotel California",
+        artist: "The Eagles",
+        year: 1976
+    },
+]
+
+const recordTitles = records.map(function (record) {
+    return record.title;
+});
