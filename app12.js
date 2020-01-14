d4b8580eeb8868585e712c291aaad301519c78b6
Marc Turner <marc.t1967@gmail.com>
2020-01-14 11:27:16 -0500

    add Dom manipulating methds

--- app12.js
+++ app12.js
@@ -1,1 +1,15 @@
-//
+//  DOM MANIPULATION
+
+const allLis = document.querySelectorAll('li');
+
+const colors = ["black", "purple", "green", "blue", "yellow", "teal"];
+
+// for (let i = 0; i <= allLis.length; i++) {
+//     allLis[i].style.color = colors[i];
+// }
+
+allLis.forEach((el, i) => {
+    const color = colors[i];
+    // console.log(el, color);
+    el.style.color = color;
+});
